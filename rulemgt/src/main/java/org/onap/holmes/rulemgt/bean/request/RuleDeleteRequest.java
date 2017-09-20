6486e95eab9ea05d91ee93bd9e254c9c3164f5a1
Guangrong Fu <fu.guangrong@zte.com.cn>
2017-09-20 21:13:40 +0800

    Add Swagger Related Configurations
    Change-Id: Ia6dddca44d18725b6b77afc5c7d5dd0ba73c3542
    Issue-ID: HOLMES-54
    Signed-off-by: Guangrong Fu <fu.guangrong@zte.com.cn>

--- rulemgt/src/main/java/org/onap/holmes/rulemgt/bean/request/RuleDeleteRequest.java
+++ rulemgt/src/main/java/org/onap/holmes/rulemgt/bean/request/RuleDeleteRequest.java
@@ -16,12 +16,16 @@
 package org.onap.holmes.rulemgt.bean.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RuleDeleteRequest {
     @JsonProperty(value = "ruleid")
     private String ruleId;
