1dacac49a9a8f22018ce84f8889b1af79fbbf859
Guangrong Fu <fu.guangrong@zte.com.cn>
2017-10-14 17:35:16 +0800

    Extract the Common Tool
    Change-Id: I5d356acd7c8747072acc74bfa5e3560cde7a8724
    Issue-ID: HOLMES-75
    Signed-off-by: Guangrong Fu <fu.guangrong@zte.com.cn>

--- rulemgt/src/main/java/org/onap/holmes/rulemgt/db/CorrelationRuleDao.java
+++ rulemgt/src/main/java/org/onap/holmes/rulemgt/db/CorrelationRuleDao.java
@@ -18,7 +18,7 @@ package org.onap.holmes.rulemgt.db;
 import java.util.List;
 import org.onap.holmes.common.api.entity.CorrelationRule;
 import org.onap.holmes.common.exception.CorrelationException;
-import org.onap.holmes.rulemgt.db.mapper.CorrelationRuleMapper;
+import org.onap.holmes.common.utils.CorrelationRuleMapper;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.BindBean;
 import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
